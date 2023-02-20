9d67568a5ae99eae6bdf320a26bf13bf2c1e9bcd
dawid-polak <dawidow.polak@gmail.com>
2023-02-20 12:57:32 +0100

    add button more in references

---
+++ portfolio-jakub/src/components/ExReference.vue
@@ -0,0 +1,63 @@
+<template>
+     <div>
+          <p v-if="show" class="text">{{ data.text }}</p>
+          <p v-else class="text">{{ data.paragraph }}</p>
+          <p class="author">{{ data.author }}</p>
+          <button @click="handleShow" class="btn">{{ btnText }}</button>
+     </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+     props: ["data"],
+     setup(props) {
+          const show = ref(false);
+          const btnText = ref("more");
+
+          const handleShow = () => {
+               show.value = !show.value;
+
+               if (btnText.value === "more") {
+                    btnText.value = "less";
+               } else {
+                    btnText.value = "more";
+               }
+          };
+
+          return { show, btnText, handleShow };
+     },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../style/style.scss";
+
+.text {
+     font-style: italic;
+     color: #929292;
+     margin-bottom: 0px;
+     display: inline-block;
+     height: 30px;
+     font-size: 14px;
+     font-weight: 200;
+     margin-left: 10px;
+}
+
+.author {
+     margin: 0px 0px 0px 10px;
+     font-size: 14px;
+     font-weight: 400;
+}
+
+.btn {
+     border: 0;
+     background-color: $yellow;
+     color: #fff;
+     padding: 6px 10px 6px 10px;
+     margin: 5px 0px 10px 10px;
+     cursor: pointer;
+     font-size: 12px;
+}
+</style>
--- portfolio-jakub/src/components/ItemExperience.vue
+++ portfolio-jakub/src/components/ItemExperience.vue
@@ -1,154 +1,124 @@
 <template>
-    <div class="item-experiences">
-        <div class="left">
-            {{ data.year }}
-        </div>
-        <div class="right">
-            <h1>{{ data.year }}</h1>
-            <h2>{{ data.company }}</h2>
-            <p>{{ data.description }}</p>
-            <div v-for="reference in data.references" :key="reference" class="references">
-                <p class="text">{{ reference.text }}</p>
-                <p class="author"> {{ reference.author }}</p>
-            </div>
-        </div>
-    </div>
+     <div class="item-experiences">
+          <div class="left">
+               {{ data.year }}
+          </div>
+          <div class="right">
+               <h1>{{ data.year }}</h1>
+               <h2>{{ data.company }}</h2>
+               <p>{{ data.description }}</p>
+              
+               <div v-for="reference in data.references" :key="reference.id" class="references">
+                    <ExReference v-if="reference.text !== ''" :data="reference"/>
+               </div>
+          </div>
+     </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref } from "vue";
+import ExReference from './ExReference.vue'
 
 export default {
-    props: ['data'],
-    setup (props) {
-        const data = ref(null);
-        data.value = props.data
+     props: ["data"],
+     components: {
+        ExReference
+     },
+     setup(props) {
+          const data = ref(null);
+          data.value = props.data;
 
-        return { data }
-    }
-}
+          //more
+
+          return { data };
+     },
+};
 </script>
 
 <style lang="scss" scoped>
-
-@import '../style/style.scss';
+@import "../style/style.scss";
 
 .item-experiences {
-    width: 100%;
-    border-top: 1px solid #929292;
-    display: flex;
+     width: 100%;
+     border-top: 1px solid #929292;
+     display: flex;
 
-    .left {
-        color: $yellow;
-        font-size: 30px;
-        font-weight: 600;
-        margin-left: 10px;
-        margin-top: 10px;
-        width: 30%;
-    }
+     .left {
+          color: $yellow;
+          font-size: 30px;
+          font-weight: 600;
+          margin-left: 10px;
+          margin-top: 10px;
+          width: 30%;
+     }
 
-    .right {
-        width: 70%;
+     .right {
+          width: 70%;
 
-        h1 {
-            display: none;
-        }
+          h1 {
+               display: none;
+          }
 
-        h2 {
-            margin-top: 15px;
-        }
+          h2 {
+               margin-top: 15px;
+          }
 
-        p {
-            font-size: 20px;
-            font-weight: 200;
-            margin-left: 10px;
-        }
+          p {
+               font-size: 20px;
+               font-weight: 200;
+               margin-left: 10px;
+          }
 
-        .references {
-            margin-left: 10px;
-
-            .text {
-                font-style: italic;
-                color: #929292;
-                margin-bottom: 0px;
-            }
-            
-            .author {
-                margin-top: 0px;
-                font-size: 16px;
-                font-weight: 400;
-            }
-
-        }
-    }
+          .references {
+               margin-left: 10px;
+          }
+     }
 }
 
+@media only screen and (max-width: 1000px) {
+     .item-experiences {
+          .right {
+               h2 {
+                    margin-top: 15px;
+                    font-size: 20px;
+               }
 
-@media only screen and (max-width: 1000px ){
-    .item-experiences {
-        .left {
-
-        }
-        .right {
-            h2 {
-                margin-top: 15px;
-                font-size: 20px;
-            }
-
-            p {
-                font-size: 18px;
-            }
-
-            .references {
-                .text {
-                    font-size: 15px;
-                }
-                .author {
-                    font-size: 13px;
-                }
-
-            }
-        }
-    }
+               p {
+                    font-size: 18px;
+               }
+          }
+     }
 }
 
-@media only screen and (max-width: 750px ){
-    .item-experiences {
-        .left {
-            display: none;
-        }
-        .right {
-            width: 95%;
+@media only screen and (max-width: 750px) {
+     .item-experiences {
+          .left {
+               display: none;
+          }
+          .right {
+               width: 95%;
 
-            h1 {
-                display: block;
-                color: $yellow;
-                margin-bottom: 0px;
-            }
+               h1 {
+                    display: block;
+                    color: $yellow;
+                    margin-bottom: 0px;
+               }
 
-            h2 {
-                margin-top: 15px;
-                margin-left: 20px;
-                font-size: 20px;
-            }
+               h2 {
+                    margin-top: 15px;
+                    margin-left: 20px;
+                    font-size: 20px;
+               }
 
-            p {
-                font-size: 18px;
-                margin-left: 40px;
-            }
+               p {
+                    font-size: 18px;
+                    margin-left: 40px;
+               }
 
-            .references {
-                margin-left: 40px;
-                .text {
-                    font-size: 15px;
-                }
-                .author {
-                    font-size: 13px;
-                }
-
-            }
-        }
-    }
+               .references {
+                    margin-left: 40px;
+               }
+          }
+     }
 }
-
 </style>
